a04767f55d6659d15f45f934e5a952c02c13573b
SteffenWDA <steffenweitkamp11qgooglemail.com>
2021-01-17 09:53:47 +0100

    addtion of code for cloudinit usage

--- main.tf
+++ main.tf
@@ -5,20 +5,22 @@ provider "aws" {
 }
 
 
+data "template_file" "user_data" {
+  template = file("scripts/add-ssh-web-app.yaml")
+}
+
 
 resource "aws_instance" "liveProjectInstance1" {
   ami = "ami-008b09448b998a562"
-  instance_type = "t2.micro"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  user_data                   = data.template_file.user_data.rendered
+
   tags = {
     Name = "liveProjectInstance1"
   }
 }
 
-
-resource "aws_instance" "liveProjectInstance2" {
-  ami = "ami-008b09448b998a562"
-  instance_type = "t2.micro"
-  tags = { #B
-    Name = "liveProjectInstance2"
-  }
+output "public_ip" {
+  value = aws_instance.liveProjectInstance1.public_ip
 }
