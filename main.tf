d9eead6233e53c280e77fccacb8945f424779b6c
SteffenWDA <steffenweitkamp11qgooglemail.com>
2021-01-17 19:46:42 +0100

    addition of second ec2 instance

--- main.tf
+++ main.tf
@@ -4,27 +4,41 @@ provider "aws" {
   region = "us-west-2"
 }
 
-
+#TODO Implement keys as resource and pass pub key here
 data "template_file" "user_data" {
   template = file("scripts/add-ssh-web-app.yaml")
 }
 
 
 resource "aws_instance" "liveProjectInstance1" {
+  #TODO: I need to implement this with filter
   ami = "ami-008b09448b998a562"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   user_data                   = data.template_file.user_data.rendered
-  security_groups = [ "allow_ssh","allow_http","allow_https"]
+  #TODO: if I access the values as attributes it causes problems during destroy security_groups = [ aws_security_group.allow_ssh.name,aws_security_group.allow_http.name,aws_security_group.allow_https.name]
+  security_groups = ["allow_ssh","allow_http","allow_https"]
   tags = {
     Name = "liveProjectInstance1"
   }
 }
 
+resource "aws_instance" "liveProjectInstance2" {
+  #TODO: I need to implement this with filter
+  ami = "ami-008b09448b998a562"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  user_data                   = data.template_file.user_data.rendered
+  #TODO: if I access the values as attributes it causes problems during destroy security_groups = [ aws_security_group.allow_ssh.name,aws_security_group.allow_http.name,aws_security_group.allow_https.name]
+  security_groups = ["allow_ssh","allow_http","allow_https"]
+  tags = {
+    Name = "liveProjectInstance2"
+  }
+}
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
-  description = "Allow TLS inbound traffic"
+  description = "aws_security_group to enable ssh"
 
   ingress {
     description = "SSH"
@@ -48,7 +62,7 @@ resource "aws_security_group" "allow_ssh" {
 
 resource "aws_security_group" "allow_http" {
   name = "allow_http"
-  description = "Allow TLS inbound traffic"
+  description = "aws_security_group to allow http"
 
   ingress {
     description = "SSH"
@@ -74,7 +88,7 @@ resource "aws_security_group" "allow_http" {
 
 resource "aws_security_group" "allow_https" {
   name = "allow_https"
-  description = "Allow TLS inbound traffic"
+  description = "aws_security_group to allow https"
 
   ingress {
     description = "SSH"
@@ -93,11 +107,23 @@ resource "aws_security_group" "allow_https" {
 
   }
   tags = {
-    Name = "allow_http"
+    Name = "allow_https"
   }
 }
 
 
-output "public_ip" {
+output "public_ip_liveProjectInstance1" {
   value = aws_instance.liveProjectInstance1.public_ip
+}
+
+output "public_dns_liveProjectInstance1" {
+  value = aws_instance.liveProjectInstance1.public_dns
+}
+
+output "public_ip_liveProjectInstance2" {
+  value = aws_instance.liveProjectInstance2.public_ip
+}
+
+output "public_dns_liveProjectInstance2" {
+  value = aws_instance.liveProjectInstance2.public_dns
 }
