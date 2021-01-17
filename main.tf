2072a884c74352be57ab80100cbd1aad8dcb2f9d
SteffenWDA <steffenweitkamp11qgooglemail.com>
2021-01-17 11:23:01 +0100

    added security group to enable login via ssh

--- main.tf
+++ main.tf
@@ -15,11 +15,39 @@ resource "aws_instance" "liveProjectInstance1" {
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   user_data                   = data.template_file.user_data.rendered
-
+  security_groups = [ "allow_ssh"]
   tags = {
     Name = "liveProjectInstance1"
   }
 }
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+
+
 
 output "public_ip" {
   value = aws_instance.liveProjectInstance1.public_ip
