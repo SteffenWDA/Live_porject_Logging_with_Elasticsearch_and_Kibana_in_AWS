f4d61c6718ba88669129b5cb298d859dcc71cf3b
SteffenWDA <steffenweitkamp11qgooglemail.com>
2021-02-07 11:37:37 +0100

    addition of outputs in root tf

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+
+output "PublicDnsInstance1" {
+  value = module.ec2.public_dns_liveProjectInstance1
+}
+
+output "PublicIpInstance1" {
+  value = module.ec2.public_ip_liveProjectInstance1
+}
+
+output "PublicDnsInstance2" {
+  value = module.ec2.public_dns_liveProjectInstance1
+}
+
+output "PublicIpInstance2" {
+  value = module.ec2.public_ip_liveProjectInstance1
+}
